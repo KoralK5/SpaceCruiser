7b7453010d72f76eb60161b92e2b0bbc303a970d
Koral <62809012+KoralK5@users.noreply.github.com>
2021-07-27 17:49:11 -0400

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+
+public class Game
+{
+    private boolean alive = true;
+	private int difficuilty;
+	private int rows = 10;
+	private int columns = 50;
+
+	public int setDifficuilty(int difficuilty) {
+	    return this.difficuilty = difficuilty; 
+	}
+
+	public void wait(int seconds) {
+		try {
+			TimeUnit.SECONDS.sleep(seconds);
+		}
+		catch(InterruptedException e) {}
+	}
+
+	public void countdown() {
+		Main.clear();
+		System.out.print("\t\t\t\t\t\t|\n".repeat(2));
+		System.out.println("\t\t\t\t\t\t3");
+		wait(1);
+
+		Main.clear();
+		System.out.print("\t\t\t\t\t\t|\n".repeat(4));
+		System.out.print("\t\t\t\t\t\t2");
+		wait(1);
+
+		Main.clear();
+		System.out.print("\t\t\t\t\t\t|\n".repeat(6));
+		System.out.print("\t\t\t\t\t\t1");
+		wait(1);
+	}
+
+	public void printStats() {
+		String diff = this.difficuilty==1 ? "easy" : (this.difficuilty==2 ? "medium" : "hard");
+		String whitespace = " - ".repeat(columns/4);
+		System.out.print("SPACE CRUISER" + whitespace);
+		System.out.print("Difficuilty: " + this.difficuilty + whitespace);
+		System.out.println("Score: " + whitespace);
+	}
+
+	public void printScreen() {
+		System.out.println("-".repeat(this.columns));
+
+		for (int i = 0; i<this.rows; i++) {
+			System.out.println("|" + " ".repeat(this.columns-2) + "|");
+		}
+
+		System.out.println("-".repeat(this.columns));
+	}
+
+    public String toString(){
+        return alive ? "Game Ongoing!" : "Game Ended!";
+    }
+}
