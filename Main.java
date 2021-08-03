a37407ab5a501732cfea3a46599b0c859e9382f6
Koral <62809012+KoralK5@users.noreply.github.com>
2021-08-03 03:33:02 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,29 +9,40 @@ import java.awt.event.KeyListener;
 class Main {
     static Scanner input = new Scanner(System.in);
     static Game game;
-
+    
+    
     public static void main(String[] args) {
+        // Using javax.swing, an interactable server graphics screen is created
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
 		frame.setFocusable(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		// A KeyListener method is called on the graphics screen frame.
 		frame.addKeyListener(new KeyListener() {
+		    // @Overrides overrides any other class methods named keyPressed, keyReleased, and keyTyped.
+
+		    // When a key is pressed, the key's integer representation is passed into the setDirection method
+		    // using the getKeyCode method along with true, indicating that it's pressed.
 			@Override
 			public void keyPressed(KeyEvent event) {
 				game.setDirection(event.getKeyCode(), true);
 			}
 
+		    // When a key is released, the key's integer representation is passed into the setDirection method
+		    // using the getKeyCode method along with false, indicating that it's released.
 			@Override
 			public void keyReleased(KeyEvent event) {
 				game.setDirection(event.getKeyCode(), false);
 			}
 
+            // This method is unnecessary, but is required for the addKeyListener function.
 			@Override
 			public void keyTyped(KeyEvent e) {
 			}
 		});
-
+        
+        // An empty panel is created to host the frame on.
 		JPanel panel = new JPanel();
 		frame.add(panel);
 
@@ -39,6 +50,7 @@ class Main {
 		while (true) {
 		    game = new Game();
 		    
+		    // Helper methods are used to introduce the game.
             clear();
             art();
             space(4);
@@ -59,23 +71,27 @@ class Main {
             clear();
             art();
             space(2);
-            game.setDifficuilty(chooseDifficuilty());
+            game.setDifficulty(chooseDifficulty());
             countdown();
 
             while (game.isOngoing()) {
-                wait(game.difficuilty*15 - game.distance/100);
+                // The game difficulty and distance are used to adjust the game speed.
+                wait(game.difficulty*15 - game.distance/100);
                 game.distance++;
-
+                
+                // Object locations are changed and displayed repetitively.
                 clear();
                 game.moveObjects();
 				game.moveSpaceship();
                 game.printScreen();
-
+                
+                // The rocket fires if it gets a present (+30 pts) or 30 points on its own.
 				if (game.score % 30 == 0) {
 					game.shoot();
 				}
     			
-                if (game.distance % game.difficuilty == 0 && Math.random() > 0.4) {
+    			// The randomness of astroid spawns are determined by the difficulty.
+                if (game.distance % game.difficulty == 0 && Math.random() > 0.4) {
                     game.score++;
                     game.addAstroid();
     				
@@ -84,12 +100,14 @@ class Main {
                     }
                 }
             }
-
+            
+            // An empty scanner is used to cancel out ghost inputs caused by the KeyListener.
 			input.nextLine();
             clear();
             art();
             space(2);
             
+            // The user's personal best is stored and used for congratulating.
             if (game.score > record) {
                 System.out.println("\t\t\tNEW RECORD!!!");
                 record = game.score;
@@ -101,6 +119,7 @@ class Main {
             System.out.print("\n\nGo again? ([Y]es or [N]o): ");
             String choice = input.nextLine().toLowerCase();
             
+            // The game ends if the user inputs "n" or "no".
             if (choice.equals("n") || choice.equals("no")) {
                 clear();
                 art();
@@ -112,15 +131,19 @@ class Main {
             }
 		}
 	}
-
+    
+    // Prints the special string to erase console text.
     public static void clear() {
 		System.out.println("\033[H\033[2J");
     }
 
+    // Prints variable lines amount of spaces by repetition using the nCopies method.
     public static void space(int lines) {
 		System.out.println(String.join("", Collections.nCopies(lines, "\n")));
     }
 
+    // Reads and prints the Spaceship.txt file line by line until there is no next line
+    // using a scaner. Returns an error message upon facing the FileNotFoundException.
     public static void art() {
 		try {
 			File ship = new File("Spaceship.txt");
@@ -138,6 +161,7 @@ class Main {
 		}
 	}
 
+    // Prints out a story.
     public static void story() {
 		System.out.println("Ever since the Earth was destroyed, humanity has been left stranded in a space cruiser with little to no resources left.\n");
         System.out.println("With the objective of searching for a sustainable planet, you set foot in your spaceship and set sail into the abyss of space...\n");
@@ -146,6 +170,7 @@ class Main {
         input.nextLine();
 	}
 
+    // Prints out the rules.
 	public static void rules() {
 		System.out.println("\t\t            THE RULES");
 		System.out.println("\t\t1: Click the graphics screen once");
@@ -160,15 +185,19 @@ class Main {
 		input.nextLine();
 	}
 
-	public static int chooseDifficuilty() {
-		System.out.println("\n\t\t\tCHOOSE DIFFICUILTY\n");
+    // Prints out the difficulties and uses the Math class to shrink down numbers from a 1-3 range.
+    // Then, adjusts the difficulty accordingly for the game.
+	public static int chooseDifficulty() {
+		System.out.println("\n\t\t\tCHOOSE DIFFICULTY\n");
 		System.out.println("\t\t\t\t1: Easy");
 		System.out.println("\t\t\t\t2: Medium");
 		System.out.println("\t\t\t\t3: Hard");
-		System.out.print("\n\nDifficuilty (1|2|3): ");
+		System.out.print("\n\nDifficulty (1|2|3): ");
 		return 12 - Math.min(3, Math.max(1, input.nextInt())) * 3;
 	}
 	
+	// Uses the java.util library's TimeUnit class to sleep variable ms miliseconds.
+	// Upon facing an error, it doesn't sleep.
 	public static void wait(int ms) {
 		try {
 			TimeUnit.MILLISECONDS.sleep(ms);
@@ -177,6 +206,7 @@ class Main {
 		}
 	}
 
+	// Uses the wait method above and some ascii art to simulate a countdown.
     public static void countdown() {
 		Main.clear();
 		System.out.print(String.join("", Collections.nCopies(2, "\t\t\t\t|\n")));
